ef55d82a4df5091f3f8a42905abdaadf23b4d6ee
johnsk18 <derpinwin@me.com>
2019-03-25 23:43:34 -0400

    2d printing with 2 ranks + ghost rows using GLIDER

--- conway2d.c
+++ conway2d.c
@@ -6,14 +6,16 @@
 #include "clcg4.h"
 #include <mpi.h>
 
-#define WIDTH 50
-#define HEIGHT 25
+#define WIDTH 64
+#define HEIGHT 32
 #define TIME 250
 #define THRESHOLD 0.25
 
 // Global Definitions
 char **curr, **next, *upGhost, *downGhost;
-int mpi_size = -1, mpi_rank = -1;
+int mpi_size = -1, mpi_rank = -1, rows_per_rank = -1;
+MPI_Request request, request2, request3, request4;
+MPI_Status status;
 
 void spawnGlider() { // spawns a glider to the board
 	curr[3][3] = 1;
@@ -63,27 +65,41 @@ void spawnGosperGliderGun() { // spawns a gosper glider gun
 }
 
 void printBoard(char** board) { // debug prints the board
-	int i, j;
-	for (i = 0; i < 50; i++) printf("\n");
-	for (j = 0; j < WIDTH; ++j) printf("%c", (upGhost[j]) ? 'X' : '.'); // prints up ghost row
-	printf("\n\n");		
-	for (i = 0; i < HEIGHT; i++) {
-		for (j = 0; j < WIDTH; j++) printf("%c", (board[i][j]) ? 'X' : '.');
-		printf("\n");
+	int i, j, r;
+	MPI_Barrier(MPI_COMM_WORLD);
+	for (r = 0; r < mpi_size; r++) {
+		if (mpi_rank == r) {
+			for (i = 0; r == 0 && i < 50; i++) printf("\n");
+			if (mpi_size != 1) printf("Rank %d\n", mpi_rank);
+			// if (mpi_size == 1) {
+				for (j = 0; j < WIDTH; ++j) printf("%c", (upGhost[j]) ? 'X' : '.'); // prints up ghost row
+				printf("\n\n");
+			// }
+			for (i = 0; i < rows_per_rank; i++) {
+				for (j = 0; j < WIDTH; j++) printf("%c", (board[i][j]) ? 'X' : '.');
+				printf("\n");
+			}
+			// if (mpi_size == 1) {
+				printf("\n");
+				for (j = 0; j < WIDTH; ++j) printf("%c", (downGhost[j]) ? 'X' : '.'); // prints down ghost row
+				printf("\n");
+			// }
+			if (mpi_size != 1) printf("\n");
+		}
+		fflush(stdout);
+		MPI_Barrier(MPI_COMM_WORLD);
 	}
-	printf("\n");
-	for (j = 0; j < WIDTH; ++j) printf("%c", (downGhost[j]) ? 'X' : '.'); // prints down ghost row
-	printf("\n");
+	MPI_Barrier(MPI_COMM_WORLD);
 }
 
 int getCell(char** board, int i, int j) {
-	if (i < -1 || i > HEIGHT) assert(-1);
+	if (i < -1 || i > rows_per_rank) assert(-1);
 
 	if (j < 0) j += WIDTH;
 	if (j >= WIDTH) j %= WIDTH;
 
 	if (i == -1) return upGhost[j]; // access up ghost row
-	else if (i == HEIGHT) return downGhost[j]; // access down ghost row
+	else if (i == rows_per_rank) return downGhost[j]; // access down ghost row
 	else return board[i][j]; // access board
 }
 
@@ -104,33 +120,31 @@ int main(int argc, char *argv[]) {
 	// Initializes variables
 
 	int i, j, t;
-	curr = (char**)calloc(HEIGHT, sizeof(char*));
-	next = (char**)calloc(HEIGHT, sizeof(int*));
-	for (i = 0; i < HEIGHT; i++) curr[i] = (char*)calloc(WIDTH, sizeof(char));
-	for (i = 0; i < HEIGHT; i++) next[i] = (char*)calloc(WIDTH, sizeof(char));
-
 	InitDefault();
-
 	MPI_Init( &argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+	rows_per_rank = HEIGHT / mpi_size;
+	curr = (char**)calloc(rows_per_rank, sizeof(char*));
+	next = (char**)calloc(rows_per_rank, sizeof(char*));
+	for (i = 0; i < rows_per_rank; i++) curr[i] = (char*)calloc(WIDTH, sizeof(char));
+	for (i = 0; i < rows_per_rank; i++) next[i] = (char*)calloc(WIDTH, sizeof(char));
 
   upGhost = (char*)calloc(WIDTH, sizeof(char));
 	downGhost = (char*)calloc(WIDTH, sizeof(char));
 
-
 	// Sets and prints the intial state of the board
 
 	#ifdef GLIDER
-		spawnGlider();
+		if (mpi_rank == 0) spawnGlider();
 	#endif
 
 	#ifdef GOSPER
-		spawnGosperGliderGun();
+		if (mpi_rank == 0) spawnGosperGliderGun();
 	#endif
 
 	#if !defined(GLIDER) && !defined(GOSPER)
-		for (i = 0; i < HEIGHT; i++) for (j = 0; j < WIDTH; j++) curr[i][j] = 1;
+		for (i = 0; i < rows_per_rank; i++) for (j = 0; j < WIDTH; j++) curr[i][j] = 1;
 	#endif
 
 	#if defined(GLIDER) || defined(GOSPER) || defined(BOARD)
@@ -140,12 +154,12 @@ int main(int argc, char *argv[]) {
 	// Running simulation
 
 	for (t = 0; t < TIME; t++) { // simulation running for TIME ticks
-		for (i = 0; i < HEIGHT; i++) {
+		for (i = 0; i < rows_per_rank; i++) {
 			for (j = 0; j < WIDTH; j++) {
 				int neighbors = getNeighbors(curr, i, j); 
 
 				#if !defined(GLIDER) && !defined(GOSPER) // RNG for non-preset conway game
-					int index = j + ((WIDTH / mpi_size) * mpi_rank); // index = local_row + (rows_per_rank * mympirank)
+					int index = j + ((WIDTH / mpi_size) * mpi_rank); // index = local_row + (rows_per_rank * mpi_rank)
 					double value = GenVal(index);
 					if (THRESHOLD >= value) {
 						next[i][j] = (value < 0.5 * THRESHOLD) ? 0 : 1;
@@ -158,27 +172,35 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		for (i = 0; i < HEIGHT; i++) for (j = 0; j < WIDTH; j++) curr[i][j] = next[i][j]; // updates board with new data
+		for (i = 0; i < rows_per_rank; i++) for (j = 0; j < WIDTH; j++) curr[i][j] = next[i][j]; // updates board with new data
 
 		if (mpi_size == 1) { // Copy the first and last rows of this rank into the ghosts
-			memcpy(upGhost, curr[HEIGHT-1], WIDTH);
+			memcpy(upGhost, curr[rows_per_rank-1], WIDTH);
 			memcpy(downGhost, curr[0], WIDTH);
 		} else {
-			
+			MPI_Irecv(upGhost, WIDTH, MPI_CHAR, (mpi_rank == 0) ? mpi_size - 1 : mpi_rank - 1, 1, MPI_COMM_WORLD, &request);
+			MPI_Isend(curr[rows_per_rank-1], WIDTH, MPI_CHAR, (mpi_rank == mpi_size - 1) ? 0 : mpi_rank + 1, 1, MPI_COMM_WORLD, &request3);
+			MPI_Irecv(downGhost, WIDTH, MPI_CHAR, (mpi_rank == mpi_size - 1) ? 0 : mpi_rank + 1, 2, MPI_COMM_WORLD, &request2);
+			MPI_Isend(curr[0], WIDTH, MPI_CHAR, (mpi_rank == 0) ? mpi_size - 1 : mpi_rank - 1, 2, MPI_COMM_WORLD, &request4);
+			MPI_Wait(&request, &status);
+			MPI_Wait(&request2, &status);
+			MPI_Wait(&request3, &status);
+			MPI_Wait(&request4, &status);
 		}
 
 
-		usleep(100*1000); // sleeps for 50 milliseconds
+		usleep(50*1000); // sleeps for 50 milliseconds
 	
 		#if defined(GLIDER) || defined(GOSPER) || defined(BOARD)
+			MPI_Barrier(MPI_COMM_WORLD);
 			printBoard(curr);
 		#endif
 	}
 
 	// Frees variables
 
-	for (i = 0; i < HEIGHT; i++) free(curr[i]);
-	for (i = 0; i < HEIGHT; i++) free(next[i]);
+	for (i = 0; i < rows_per_rank; i++) free(curr[i]);
+	for (i = 0; i < rows_per_rank; i++) free(next[i]);
 	free(curr);
 	free(next);
 	free(upGhost);
